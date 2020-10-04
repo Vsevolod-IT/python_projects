9c8e1943c35e073258125ba5548391307798e7ff
Vsevolod <it_vsevolod@yahoo.com>
2020-10-04 21:00:48 +0300

    add starting field

---
+++ small_games/the_gallows.py
@@ -0,0 +1,16 @@
+'''a =[' |@-----@\n'
+     '|      |\n
+     '|      0\n
+     '|     /|\\n
+      |     / \\n
+     /|\' ]'''
+import itertools
+
+a = [[i for i in itertools.repeat('.', 30)]for i in range(15)]
+for i in a:
+     print(*i)
+
+
+
+
+
