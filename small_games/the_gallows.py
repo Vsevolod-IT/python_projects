5dc54a44eea2cd94a1035892df8c270917178791
Vsevolod <it_vsevolod@yahoo.com>
2020-10-04 21:25:00 +0300

    save point

--- small_games/the_gallows.py
+++ small_games/the_gallows.py
@@ -10,7 +10,10 @@ a = [[i for i in itertools.repeat('.', 30)]for i in range(15)]
 for i in a:
      print(*i)
 
-
+hangman_design = {'base':'/|\ ',
+                  'post': '|',
+                  'beam': '=++=+=+=+=+=+@'}
+print(hangman_design)
 
 
 
