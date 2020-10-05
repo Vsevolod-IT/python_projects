f785e167294d7937b6811c80e1b5a590178fe61e
Vsevolod <it_vsevolod@yahoo.com>
2020-10-05 20:58:17 +0300

    part for paint,  the next step put it into func

--- small_games/the_gallows.py
+++ small_games/the_gallows.py
@@ -1,19 +1,54 @@
-'''a =[' |@-----@\n'
-     '|      |\n
-     '|      0\n
-     '|     /|\\n
-      |     / \\n
-     /|\' ]'''
-import itertools
 
-a = [[i for i in itertools.repeat('.', 30)]for i in range(15)]
+part = {''}
+
+# create screen
+a = [['.'] * 15 for i in range(10)]
+
+# create vertical
+for line,colone in enumerate(a):
+    if (0 < line < 9):
+        for colone,i in enumerate(colone):
+            if colone == 3:
+                 a[line][colone] = 'N'
+
+# create gorizontal
+for line,colone in enumerate(a):
+    if line == 1:
+        for colone,i in enumerate(colone):
+            if (3 < colone < 8):
+                 a[line][colone] = '='
+
+# create bond
+for line,colone in enumerate(a):
+    if (1 < line < 4):
+        for colone,i in enumerate(colone):
+            if colone == 7:
+                 a[line][colone] = '|'
+
+# block create man
+for line,colone in enumerate(a):
+    if line == 4:
+        for colone,i in enumerate(colone):
+            if colone == 7:
+                 a[line][colone] = '@'
+
+
+for line,colone in enumerate(a):
+    if line == 5:
+        for colone,i in enumerate(colone):
+            if colone == 7:
+                 a[line][colone] = '/|\\'
+
+for line,colone in enumerate(a):
+    if line == 6:
+        for colone,i in enumerate(colone):
+            if colone == 7:
+                 a[line][colone] = '/\\'
+# view screen
 for i in a:
-     print(*i)
+    print(*i)
 
-hangman_design = {'base':'/|\ ',
-                  'post': '|',
-                  'beam': '=++=+=+=+=+=+@'}
-print(hangman_design)
+
 
 
 
