39a913a8a5b38deb33f1d9637117240ea5ac8f4e
Vsevolod <it_vsevolod@yahoo.com>
2020-10-06 16:31:50 +0300

    prototype func for display and paint

--- small_games/the_gallows.py
+++ small_games/the_gallows.py
@@ -2,51 +2,73 @@
 
 
 # create screen
-a = [[' '] * 15 for i in range(10)]
+def create_screen(symbol):
+    return [[symbol] * 15 for i in range(10)]
+a = create_screen(' ')
 
 # create vertical
-for line,colone in enumerate(a):
-    if (0 < line < 9):
-        for colone,i in enumerate(colone):
-            if colone == 3:
-                 a[line][colone] = 'N'
+def create_post(a):
+    for line,colone in enumerate(a):
+        if (0 < line < 9):
+            for colone,i in enumerate(colone):
+                if colone == 3:
+                     a[line][colone] = 'N'
+    return a
+create_post(a)
 
 # create gorizontal
-for line,colone in enumerate(a):
-    if line == 1:
-        for colone,i in enumerate(colone):
-            if (3 < colone < 8):
-                 a[line][colone] = '='
+def create_beam(a):
+    for line,colone in enumerate(a):
+        if line == 1:
+            for colone,i in enumerate(colone):
+                if (3 < colone < 8):
+                     a[line][colone] = '='
+    return a
 
-# create bond
-for line,colone in enumerate(a):
-    if (1 < line < 4):
-        for colone,i in enumerate(colone):
-            if colone == 7:
-                 a[line][colone] = ' |'
+create_beam(a)
+# create rope
+def create_rope(a):
+    for line,colone in enumerate(a):
+        if (1 < line < 4):
+            for colone,i in enumerate(colone):
+                if colone == 7:
+                     a[line][colone] = ' |'
+    return a
+create_rope(a)
 
 # block create man
-for line,colone in enumerate(a):
-    if line == 4:
-        for colone,i in enumerate(colone):
-            if colone == 7:
-                 a[line][colone] = ' @'
+def create_head(a):
+    for line,colone in enumerate(a):
+        if line == 4:
+            for colone,i in enumerate(colone):
+                if colone == 7:
+                     a[line][colone] = ' @'
+    return a
+create_head(a)
 
+def create_body(a):
+    for line,colone in enumerate(a):
+        if line == 5:
+            for colone,i in enumerate(colone):
+                if colone == 7:
+                     a[line][colone] = '/()\\'
+    return a
+create_body(a)
 
-for line,colone in enumerate(a):
-    if line == 5:
-        for colone,i in enumerate(colone):
-            if colone == 7:
-                 a[line][colone] = '/()\\'
-
-for line,colone in enumerate(a):
-    if line == 6:
-        for colone,i in enumerate(colone):
-            if colone == 7:
-                 a[line][colone] = ' /\\'
-# view screen
-for i in a:
-    print(*i)
+def create_legs(a):
+    for line,colone in enumerate(a):
+        if line == 6:
+            for colone,i in enumerate(colone):
+                if colone == 7:
+                     a[line][colone] = ' /\\'
+    return a
+create_legs(a)
+# display game's screen
+def display_screen(a):
+    for i in a:
+        print(*i)
+    return a
+display_screen(a)
 
 
 
